1db640e535dd77e81109db8ac7b05352c1424f10
u4bi <u4bi@u4bi.com>
2017-10-02 17:58:33 +0900

    hostlistener keycap event render
    - box-shadow color

--- src/app/main/main-keyboard/main-keyboard.component.ts
+++ src/app/main/main-keyboard/main-keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-keyboard',
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainKeyboardComponent implements OnInit {
 
+  private textArray : string[];
+
   constructor() { }
 
   ngOnInit() {
+    let text = 'ӨСЬВЗАШЕЭНГЮЧРЁУҮЖЯХПЩЙИТЫФЦЛДОМБ';
+    this.textArray = text.split("");
+
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  onKeyDown(ev:KeyboardEvent) {
+         
+    let key = document.querySelector(`div[data-key="${ev.keyCode}"]`);
+    if(!key) return;
+
+    // key.classList.add('send-o');
+    key.classList.add('send-x');
+
+  }
+
+
+  @HostListener('document:keyup', ['$event'])
+  onKeyUp(ev:KeyboardEvent) {
+         
+    let key = document.querySelector(`div[data-key="${ev.keyCode}"]`);
+    if(!key) return;
+
+    // key.classList.remove('send-o');
+    key.classList.remove('send-x');
+
   }
 
 }
